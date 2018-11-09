acf206e7ea99aa0bdcf94ab5edceac8b79f349ad
Memologist <nico@ruest.com>
2018-11-09 19:25:46 +0100

    Connected fron- & backend

---
+++ AimDatasaver.php
@@ -0,0 +1,13 @@
+<?php
+    //$variablename = "ich bin der inhalt";
+    //echo $variablename;
+
+    //var_dump($_POST);
+    $highScore = $_POST['highScore'];
+    $userName = $_POST['userName'];
+
+    //in datenbank schreiben
+
+    //erfolgsmeldung zurückgeben
+
+?>
